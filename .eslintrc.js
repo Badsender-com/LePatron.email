ff0e1265c6e9830ec1533ec64e250eccee65b971
Renan <renan.decamps@gmail.com>
2021-02-12 11:19:12 +0100

    feat: add linters config

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:vue/essential",
+    "prettier"
+  ],
+  "parserOptions": {
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "vue"
+  ],
+  "rules": {
+  }
+};
