91bb36b901ec7b41c5c81ba548c4eca71deda755
Omar borji <omar@bearstudio.fr>
2021-03-24 12:09:27 +0100

    create tags

--- packages/ui/routes/mailings/__partials/mailings-tags-menu.vue
+++ packages/ui/routes/mailings/__partials/mailings-tags-menu.vue
@@ -1,8 +1,12 @@
 <script>
+import { validationMixin } from 'vuelidate';
+import { required } from 'vuelidate/lib/validators';
+
 const CHECKBOX_UNCHECKED = 'check_box_outline_blank';
 const CHECKBOX_CHECKED = 'check_box';
 export default {
   name: 'BsMailingsTagsMenu',
+  mixins: [validationMixin],
   props: {
     tags: { type: Array, default: () => [] },
     selectedMailTags: { type: Array, default: () => [] },
@@ -10,14 +14,21 @@ export default {
   data() {
     return {
       showTagMenu: false,
+      newTagDialog: false,
+      newTagName: '',
       addedTags: [],
+      newTags: [],
       removedTags: [],
     };
   },
-
+  validations() {
+    return {
+      newTagName: { required },
+    };
+  },
   computed: {
     tagsCheckboxList() {
-      return this.tags.map((tagName) => {
+      return [...this.tags, ...this.newTags].map((tagName) => {
         if (this.addedTags.includes(tagName)) {
           return { name: tagName, checkIcon: CHECKBOX_CHECKED };
         }
@@ -26,6 +37,13 @@ export default {
         }
         return { name: tagName, checkIcon: CHECKBOX_UNCHECKED };
       });
+    },
+    tagNameErrors() {
+      const errors = [];
+      if (!this.$v?.newTagName?.$dirty) return errors;
+      !this.$v?.newTagName?.required &&
+        errors.push(this.$t('global.errors.nameRequired'));
+      return errors;
     },
   },
   watch: {
@@ -49,6 +67,21 @@ export default {
     },
     openMenu() {
       this.showTagMenu = true;
+    },
+    closeNewTagDialog() {
+      this.newTagDialog = false;
+    },
+    openNewTagDialog() {
+      this.newTagDialog = true;
+    },
+    onCreateNewTag() {
+      this.$v.$touch();
+      if (this.$v.$invalid) return;
+      const { newTagName } = this;
+      if (![...this.tags, ...this.newTags].includes(newTagName)) {
+        this.newTags.push(newTagName);
+      }
+      this.closeNewTagDialog();
     },
     toggleTag(tagCheckbox) {
       const { checkIcon: tagStatus, name: tagName } = tagCheckbox;
@@ -74,41 +107,76 @@ export default {
 </script>
 
 <template>
-  <v-dialog v-model="showTagMenu" :close-on-content-click="false" :width="300">
-    <v-card>
-      <v-list>
-        <v-list-item>
-          <v-list-item-content>
-            <v-list-item-title class="title">
-              {{ $t(`tags.list`) }}
-            </v-list-item-title>
-          </v-list-item-content>
-          <v-list-item-action>
-            <v-btn icon @click="closeMenu">
-              <v-icon>close</v-icon>
-            </v-btn>
-          </v-list-item-action>
-        </v-list-item>
-      </v-list>
-      <v-divider />
-      <v-list>
-        <v-list-item
-          v-for="tagCheckbox in tagsCheckboxList"
-          :key="tagCheckbox.name"
-          @click="toggleTag(tagCheckbox)"
-        >
-          <v-list-item-action>
-            <v-icon>{{ tagCheckbox.checkIcon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-title>{{ tagCheckbox.name }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
-      <v-card-actions>
-        <v-spacer />
-        <v-btn color="primary" @click="onUpdateMailingsTags">
-          {{ $t(`global.apply`) }}
-        </v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+  <div>
+    <v-dialog
+      v-model="showTagMenu"
+      :close-on-content-click="false"
+      :width="300"
+    >
+      <v-card>
+        <v-list>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="title">
+                {{ $t(`tags.list`) }}
+              </v-list-item-title>
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-btn icon @click="closeMenu">
+                <v-icon>close</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
+        <v-divider />
+        <v-list>
+          <v-list-item
+            v-for="tagCheckbox in tagsCheckboxList"
+            :key="tagCheckbox.name"
+            @click="toggleTag(tagCheckbox)"
+          >
+            <v-list-item-action>
+              <v-icon>{{ tagCheckbox.checkIcon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-title>{{ tagCheckbox.name }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" text @click="openNewTagDialog">
+            {{ $t('tags.new') }}
+          </v-btn>
+          <v-btn color="primary" @click="onUpdateMailingsTags">
+            {{ $t(`global.apply`) }}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-dialog v-model="newTagDialog" width="500">
+      <v-card>
+        <v-card-title class="headline">
+          {{ $t('tags.new') }}
+        </v-card-title>
+        <v-card-text>
+          <v-text-field
+            v-model="newTagName"
+            :label="$t('global.name')"
+            :error-messages="tagNameErrors"
+            @input="$v.newTagName.$touch()"
+            @blur="$v.newTagName.$touch()"
+          />
+        </v-card-text>
+        <v-divider />
+        <v-card-actions>
+          <v-spacer />
+          <v-btn text @click="closeNewTagDialog">
+            {{ $t('global.cancel') }}
+          </v-btn>
+          <v-btn color="primary" @click="onCreateNewTag">
+            {{ $t('global.create') }}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
