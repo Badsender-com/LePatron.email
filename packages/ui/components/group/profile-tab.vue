6634da9d6c8149e4c42f6078b5e74f9423537326
Omar borji <omar@bearstudio.fr>
2021-06-16 14:44:00 +0100

    feat: init listing profils

--- packages/ui/components/group/profile-tab.vue
+++ packages/ui/components/group/profile-tab.vue
@@ -1,11 +1,49 @@
 <script>
+import BsProfilesTable from '~/components/profiles/table.vue';
+
+const timeout = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const profilesData = [
+  {
+    id: 0,
+    name: 'Profil 1',
+    type: 'actity',
+    createdAt: '16/06/2021',
+  },
+  {
+    id: 2,
+    name: 'Profil 2',
+    type: 'sendingblue',
+    createdAt: '14/06/2021',
+  },
+  {
+    id: 3,
+    name: 'Profil 3',
+    type: 'sendingblue',
+    createdAt: '10/06/2021',
+  },
+];
 export default {
   name: 'BsGroupProfilesTab',
+  components: {
+    BsProfilesTable,
+  },
+  data() {
+    return { profiles: [], loading: false };
+  },
+  async mounted() {
+    this.loading = true;
+    await timeout(1000);
+    this.profiles = profilesData;
+    this.loading = false;
+  },
 };
 </script>
 
 <template>
   <v-card flat tile>
-    <v-card-text> Profile listing </v-card-text>
+    <v-card-text>
+      <bs-profiles-table :profiles="profiles" :loading="loading" />
+    </v-card-text>
   </v-card>
 </template>
