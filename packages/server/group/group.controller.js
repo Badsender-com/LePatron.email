d04c4d9b019640e04dea91400fcd7c099507838c
amineziraoui <amine@bearstudio.fr>
2021-09-07 10:37:49 +0100

    Fix build use error

--- packages/server/group/group.controller.js
+++ packages/server/group/group.controller.js
@@ -160,7 +160,7 @@ async function readTemplates(req, res) {
  *
  * @apiParam {string} groupId
  *
- * @apiUse emailGroups
+ * @apiUse emailGroup
  * @apiSuccess {emailGroup[]} items list of email groups
  */
 
