6db68962a76d66ef4b8892dc48b5d7e134adbf98
amineziraoui <amine@bearstudio.fr>
2021-10-06 09:34:51 +0100

    Change wording editor

--- public/lang/badsender-fr.js
+++ public/lang/badsender-fr.js
@@ -8,12 +8,15 @@ module.exports = {
   'edit-title-ajax-pending': 'Changement du nom…',
   'edit-title-ajax-success': 'Mise à jour du nom effectuée',
   'edit-title-ajax-fail': "Impossible d'enregistrer le nouveau nom",
+
   // empty title fallback
   'title-empty': 'sans titre',
-  //
-  'save-message-success': "L'email a été sauvé",
+
+  // save
+  'save-message-success': "L'email a été sauvegardé",
   'save-message-error': "Une erreur est survenue lors de l'enregistrement",
-  //
+
+  // gallery
   'gallery-title': 'Galeries :',
   'gallery-mailing': "SPÉCIFIQUE À L'EMAIL",
   'gallery-mailing-loading': "Chargement de la galerie de l'email…",
@@ -21,18 +24,22 @@ module.exports = {
   'gallery-template': 'COMMUN AU TEMPLATE',
   'gallery-template-loading': 'Chargement de la galerie du template…',
   'gallery-template-empty': 'La galerie du template est vide',
-  'gallery-remove-image-success': "L'image a bien été enlevée de la galerie",
+  'gallery-remove-image-success': "L'image a bien été supprimée de la galerie",
   'gallery-remove-image-fail':
     "Une erreur est survenue lors de la suppression de l'image",
+
   // bgimage widget
-  'widget-bgimage-button': 'choisissez une image',
-  'widget-bgimage-reset': "enlever l'image",
+  'widget-bgimage-button': 'Choisissez une image',
+  'widget-bgimage-reset': "Enlever l'image",
+
   // prevent i18n console.warn
   'Fake image editor': '',
   '<p>Fake image editor</p>': '',
+
   // download button
   'dl-btn-regular': 'Téléchargement local',
   'dl-btn-cdn': 'images ICOU',
+
   // Crop interface
   'rotate-left': 'Incliner à gauche',
   'rotate-right': 'Incliner à droite',
@@ -41,36 +48,37 @@ module.exports = {
   'error-server':
     "Une erreur s'est produite lors de l'appel de l'API du serveur",
   cancel: 'ANNULER',
-  upload: 'UPLOADER',
+  upload: 'ENVOYER',
 
   // Profile form esp
   'sender-name': "Nom de l'expéditeur",
   'sender-mail': "Adresse email de l'expéditeur",
   replyto: 'Adresse email de réponse',
-  mailSubject: 'Objet du mail',
+  mailSubject: "Objet de l'mail",
   templateSubject: 'Objet du template',
   name: 'Nom',
-  mailName: 'Nom du mail',
+  mailName: "Nom de l'email",
   templateName: 'Nom du template',
   'export-to': 'Exporter vers',
-  exporting: 'Exportation en cours ...',
-  loading: 'Loading',
+  exporting: 'Exportation…',
+  loading: 'Chargement',
   submit: 'Enregistrer',
-  close: 'Fermer',
-  export: 'Exporter',
+  close: 'FERMER',
+  export: 'EXPORTER',
   'warning-esp-message':
-    'Toute mise à jour remplacera le mail dans le fournisseur de services de messagerie',
+    "Toute mise à jour remplacera l'email dans votre routeur",
+
   // profile form validation
-  'mail-name-required': 'Le nom du mail est requis',
+  'mail-name-required': "Le nom de l'email est requis",
   'template-name-required': 'Le nom du template est requis',
-  'mail-subject-required': "L'Objet du mail est requis",
-  'template-subject-required': "L'Objet du template est requis",
-  'mail-success-esp-send': 'Mail exporté avec succès',
+  'mail-subject-required': "L'objet de l'email est requis",
+  'template-subject-required': "L'objet du template est requis",
+  'mail-success-esp-send': 'Email exporté avec succès',
   'template-success-esp-send': 'Template exporté avec succès',
   'error-server-400':
-    "Paramètres Esp invalides, vérifiez si le courrier de l'expéditeur correspond à la clé API",
+    "Paramètres ESP invalides. Vérifiez si l'adresse email de l'expéditeur correspond à la clé API",
   'error-server-402': "Échec de l'exportation, l'ESP exige un paiement",
-  'error-server-409': 'Nom de mail déjà utilisé',
+  'error-server-409': "Nom d'email déjà utilisé",
   'error-server-500':
     "Une erreur s'est produite lors de l'appel de l'API du serveur",
   'supported-language': 'Langue',
@@ -78,14 +86,14 @@ module.exports = {
   'encoding-type': "Type d'encodage",
   entity: 'Entité',
   'title-send-test-mails': 'Envoyer un email de test',
-  'send-test-success': 'Mail envoyé avec succès',
-  'send-test-error': "Error lors de l'envoie du mail",
+  'send-test-success': 'Email envoyé avec succès',
+  'send-test-error': "Erreur lors de l'envoi de l'email",
   'placeholder-input-emails-test':
-    'Example: premieremail@test.com;secondemail@test.com',
-  'emails-test': 'Saisir un ou plusieurs emails',
+    'Example : premieremail@test.com;secondemail@test.com',
+  'emails-test': 'E-mails de test',
   'emails-invalid':
     'Les emails sont invalides, veuillez saisir des emails valides séparer par ;',
-  'placeholder-emails-groups': 'Sélectionnez une liste',
-  'sending-test-mails': "Envoi de l'email de test...",
-  'send-test-mails': " Envoyer l'email de test",
+  'placeholder-emails-groups': 'Sélectionnez une liste de test',
+  'sending-test-mails': "Envoi d'un email de test…",
+  'send-test-mails': 'Envoyer un email de test',
 };
