927908fcb2eec0e628c47efdb95ea386adf9608c
Florian <florian@bearstudio.fr>
2022-11-08 18:31:56 +0100

    feat: add input in editor for tracking management

--- packages/editor/src/js/vue/trackingParamsPlugin.js
+++ packages/editor/src/js/vue/trackingParamsPlugin.js
@@ -13,8 +13,20 @@ module.exports = {
       mounted() {
       },
       template: `
-        <div>
-          Code goes here
+        <div class="objLabel level1">
+          <span class="objLabel level1">
+            <span data-bind="text: $root.ut('template', 'Tracking')">Tracking</span>
+          </span>
+          <div class="propEditor ">
+            <span class="propLabel">
+              <span data-bind="text: $root.ut('template', 'Tracking')">Tracking URL</span>
+            </span>
+            <div class="propInput">
+              <label>
+                <input type="text" />
+              </label>
+            </div>
+          </div>
         </div>
       `,
     });
