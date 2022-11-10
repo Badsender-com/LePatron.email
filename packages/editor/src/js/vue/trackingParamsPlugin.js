e967be3fde6b2c87ddddff1deacbc1f7279d7f53
Florian <florian@bearstudio.fr>
2022-11-10 18:14:47 +0100

    fix: trackingUrl persistence when saving

--- packages/editor/src/js/vue/trackingParamsPlugin.js
+++ packages/editor/src/js/vue/trackingParamsPlugin.js
@@ -1,29 +1,57 @@
 const Vue = require('vue/dist/vue.common');
 
 module.exports = {
-  viewModel(vm, ko) {},
+  viewModel(vm, ko) {
+    const trackingUrl = ko.observable(null);
+    console.log({ content: vm.content() });
+    vm.content({ ...vm.content(), trackingUrl: trackingUrl })
+  },
   init(vm) {
     // Init VueJS component
     Vue.component('TrackingParamsPlugin', {
       components: {
       },
       data: () => ({
-        viewModel: vm,
+        // subscriptionTrackingUrl: null,
+        trackingUrl: vm.content().trackingUrl(),
       }),
       mounted() {
+        // this.subscriptionTrackingUrl = vm.content().trackingUrl().subscribe(this.updateTrackingUrl)
+      },
+      // beforeDestroy() {
+      //   this.subscriptionTrackingUrl.dispose();
+      // },
+      watch: {
+        trackingUrl(newTrackingUrl) {
+          vm.content().trackingUrl(newTrackingUrl);
+        }
+      },
+      methods: {
+        clickTemplate(obj) {
+          console.log(obj);
+          console.log({ content: vm.content()});
+        },
+        // updateTrackingUrl(data) {
+        //   console.log({ data });
+        //   console.log({ trackingUrl: this.viewModel.content().trackingUrl() });
+        //   this.trackingUrl = data
+        // }
       },
       template: `
-        <div class="objLabel level1">
+        <div class="objEdit level1">
           <span class="objLabel level1">
             <span data-bind="text: $root.ut('template', 'Tracking')">Tracking</span>
           </span>
-          <div class="propEditor ">
+          <div class="propEditor">
             <span class="propLabel">
               <span data-bind="text: $root.ut('template', 'Tracking')">Tracking URL</span>
             </span>
             <div class="propInput">
               <label>
-                <input type="text" />
+                <input
+                  type="text"
+                  v-model="trackingUrl"
+                />
               </label>
             </div>
           </div>
