5be3d0cf19ef0ef568c57a00fca0ad309eff7541
hguillaume <heloise.guillaume@bearstudio.fr>
2025-08-25 10:50:08 +0200

    fix: no ftp config necessary whith adobe profile

--- packages/ui/components/profiles/profile-form.vue
+++ packages/ui/components/profiles/profile-form.vue
@@ -49,6 +49,9 @@ export default {
     selectedEspName() {
       return this.selectedEsp + 'Component';
     },
+    needsFtpConfig() {
+      return this.selectedEsp !== ESP_TYPES.ADOBE;
+    },
   },
   async mounted() {
     if (this.profile.type) {
@@ -82,7 +85,7 @@ export default {
       {{ title }}
     </v-card-title>
     <v-card-text>
-      <v-row v-if="!group.downloadMailingWithFtpImages">
+      <v-row v-if="needsFtpConfig && !group.downloadMailingWithFtpImages">
         <v-col cols="12">
           <v-alert
             dense
