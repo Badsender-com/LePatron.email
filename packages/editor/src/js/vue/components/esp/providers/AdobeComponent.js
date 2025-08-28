fb40487d26d1b752591a5f195a694892affe32eb
hguillaume <heloise.guillaume@bearstudio.fr>
2025-08-28 12:23:08 +0200

    fix: première ouverture de la modale n'est plus vi de

--- packages/editor/src/js/vue/components/esp/providers/AdobeComponent.js
+++ packages/editor/src/js/vue/components/esp/providers/AdobeComponent.js
@@ -35,15 +35,22 @@ const AdobeComponent = Vue.component('AdobeComponent', {
       isDeliveryLoading: false,
     };
   },
+  watch: {
+    fetchedFolders: {
+      immediate: true,
+      handler(newFolders) {
+        this.folders = buildTreeFromFolders(newFolders);
+        this.$nextTick(() => {
+          const folderTree = document.getElementById('folder-tree');
+          if (folderTree) folderTree.dataSource = this.folders;
+        });
+      }
+    }
+  },
   async mounted() {
     this.profile.campaignMailName = this.vm.creationName();
     this.isFolderLoading = false;
-    this.folders = buildTreeFromFolders(this.fetchedFolders);
 
-    const folderTree = document.getElementById('folder-tree');
-    if (folderTree) {
-      folderTree.dataSource = this.folders;
-    }
     M.updateTextFields();
     window.addEventListener("beforeunload", this.handleBeforeUnload);
   },
